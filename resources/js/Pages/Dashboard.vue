b67225d8f941c0e8eed66355f65a8b04bd92d1e3
bimmbin <bimmbin.gomez@gmail.com>
2024-06-01 21:43:55 +0800

    dev branch

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -21,7 +21,7 @@ import { Head } from "@inertiajs/vue3";
                     class="overflow-hidden bg-white shadow-sm dark:bg-gray-800 sm:rounded-lg"
                 >
                     <div class="p-6 text-gray-900 dark:text-gray-100">
-                        You're logged in! asdfsadfsd
+                        You're logged in! dev
                     </div>
                 </div>
             </div>
